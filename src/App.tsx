c67970375abe76013cefa03faa347e5969f7bb89
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-02-22 17:00:55 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,38 @@ import Resume from "./components/Resume.tsx";
 
 function App() {
   const [repos, setRepos] = useState([]);
+  const [activeLink, setActiveLink] = useState("projects");
+
+  useEffect(() => {
+    if (location.pathname === "/resume") {
+      setActiveLink("resume");
+    } else {
+      setActiveLink("projects");
+    }
+  }, [location.pathname]);
+
+  const handleSetActiveLink = (link) => {
+    setActiveLink(link);
+  };
 
   const api = "https://api.github.com/users/okeke-divine/repos?sort=pushed";
+  const socials = [
+    {
+      name: "Github",
+      link: "https://github.com/Okeke-Divine/",
+      icon: <i className="fab fa-github"></i>,
+    },
+    {
+      name: "Linkedin",
+      link: "https://www.linkedin.com/in/okeke-divine-vessel/",
+      icon: <i className="fab fa-linkedin"></i>,
+    },
+    {
+      name: "Email",
+      link: "mailto:okekedivine.skiy1@gmail.com",
+      icon: <i className="fab fa-at"></i>,
+    },
+  ];
 
   useEffect(() => {
     const fetchRepos = async () => {
@@ -30,86 +60,98 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <div className="flex flex-wrap w-full p-6 md:p-[100px] md:flex md:flex-wrap md:justify-between">
-        <div className="w-full md:w-1/2 md:block  pb-2 md:pb-0">
-          <div className="md:w-4/5">
-            <div className="text-white font-bold text-6xl">Okeke Divine</div>
-            <div className="text-white font-semibold text-2xl pt-5">
-              Full Stack Web Developer
-            </div>
-            <div className="pt-10 text-gray-400 line-height-2">
-              Hello, I'm <span className="text-white">Divine</span>, a full
-              stack web developer. I love{" "}
-              <span className="text-white">problem solving</span> and critical
-              thinking, I live for the challenge, I use coding as a tool to{" "}
-              <span className="text-white">solve problems</span>. I am currently
-              interested in{" "}
-              <span className="text-white">building interactive</span> UI's for
-              game development.
-            </div>
-            <div className="pt-10">
-              <BrowserRouter>
-              <Link to="/">
-                <div className="nav-link-active flex group items-center text-gray-400 hover:text-white gap-3 w-fit justify-items-center cursor-pointer text-xs tracking-[.2em]">
-                  <div>00</div>
-                  <div>
-                    <div className="bg-white w-6 h-[1px] group-hover:w-16 duration-300"></div>
+    <BrowserRouter>
+      <div>
+        <div className="flex flex-wrap w-full p-6 md:p-[100px] md:flex md:flex-wrap md:justify-between">
+          <div className="w-full md:w-1/2 md:block  pb-10 md:pb-0">
+            <div className="md:w-4/5">
+              <div className="flex justify-between items-center">
+                <div className="text-white font-bold text-6xl">
+                  Okeke Divine
+                </div>
+                <div className="md:hidden block text-gray-400 text-2xl">
+                  <div className="cursor-pointer">
+                    <i className="fa-solid fa-bars-staggered"></i>
                   </div>
-                  <div className="uppercase">Projects</div>
                 </div>
-              </Link>
-              <Link to="/resume">
-                <div className="flex group pt-10 items-center text-gray-400 hover:text-white gap-3 w-fit justify-items-center cursor-pointer text-xs tracking-[.2em]">
-                  <div>01</div>
-                  <div>
-                    <div className="bg-white w-6 h-[1px] group-hover:w-16 duration-300"></div>
+              </div>
+              <div className="text-white font-semibold text-2xl pt-5">
+                Full Stack Web Developer
+              </div>
+              <div className="pt-10 text-gray-400 line-height-2 text-[1rem]">
+                Hello, I'm <span className="text-white">Divine</span>, a full
+                stack web developer. I love{" "}
+                <span className="text-white">problem solving</span> and critical
+                thinking, I live for the challenge, I use coding as a tool to{" "}
+                <span className="text-white">solve problems</span>. I am
+                currently interested in{" "}
+                <span className="text-white">building interactive</span> UI's
+                for game development.
+              </div>
+              <div className="pt-10 hidden md:block">
+                <Link to="/" onClick={() => handleSetActiveLink("projects")}>
+                  <div
+                    className={`${
+                      activeLink === "projects" ? "nav-link-active" : ""
+                    } flex group items-center text-gray-400 hover:text-white gap-3 w-fit justify-items-center cursor-pointer text-xs tracking-[.2em]`}
+                  >
+                    <div>00</div>
+                    <div>
+                      <div className="bg-white w-6 h-[1px] group-hover:w-16 duration-300"></div>
+                    </div>
+                    <div className="uppercase">Projects</div>
                   </div>
-                  <div className="uppercase">Resume</div>
-                </div>
-              </Link>
-              </BrowserRouter>  
-            </div>
-            <div className="pt-[6.8rem]">
-              <div className="flex flex-wrap gap-3">
-                <div className="text-white flex gap-2 items-center">
-                  <span>
-                    <i className="fab fa-github"></i>
-                  </span>
-                  Github
-                </div>
-                <div className="text-white flex gap-2 items-center">
-                  <span>
-                    <i className="fab fa-linkedin"></i>
-                  </span>
-                  Linkedin
-                </div>
-                <div className="text-white flex gap-2 items-center">
-                  <span>
-                    <i className="fa-solid fa-at"></i>
-                  </span>
-                  Email
+                </Link>
+                <Link
+                  to="/resume"
+                  onClick={() => handleSetActiveLink("resume")}
+                >
+                  <div
+                    className={`${
+                      activeLink === "resume" ? "nav-link-active" : ""
+                    } flex group pt-10 items-center text-gray-400 hover:text-white gap-3 w-fit justify-items-center cursor-pointer text-xs tracking-[.2em]`}
+                  >
+                    <div>01</div>
+                    <div>
+                      <div className="bg-white w-6 h-[1px] group-hover:w-16 duration-300"></div>
+                    </div>
+                    <div className="uppercase">Resume</div>
+                  </div>
+                </Link>
+              </div>
+              <div className="pt-3 md:pt-[6.8rem]">
+                <div className="flex flex-wrap gap-3">
+                  {socials.map((social, index) => (
+                    <a
+                      key={index}
+                      target="_blank"
+                      href={social.link}
+                      className="cursor-pointer"
+                    >
+                      <div className="text-white flex gap-2 items-center">
+                        <span>{social.icon}</span>
+                        {social.name}
+                      </div>
+                    </a>
+                  ))}
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="w-full md:w-1/2 max-h-full md:max-h-100vh overflow-auto md:overflow-visible">
-          <div className="md:w-[90%]">
-            {/* <Resume /> */}
-            {/* <Projects repos={repose}/> */}
-            <BrowserRouter>
+          <div className="w-full md:w-1/2">
+            <div className="md:w-[90%]">
+              {/* <Resume /> */}
+              {/* <Projects repos={repose}/> */}
               <Routes>
                 <Route path="/" element={<Projects repos={repos} />} />
                 <Route path="/resume" element={<Resume />} />
               </Routes>
-            </BrowserRouter>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
