90296f4d4d5ef24cf196c0a551966b951a1f46e2
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-02-23 19:53:42 +0100

    updated

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -25,13 +25,15 @@ export default function Projects(props: ProjectsProps) {
   const repos = props.repos;
   const [loadingComponent, setLoadingComponent] = useState("Loading...");
   const [totalRepos, setTotalRepos] = useState("-");
-  const [firstTimeLoading,setFirstTimeLoading] = useState(true);
+  const [firstTimeLoading, setFirstTimeLoading] = useState(true);
   useEffect(
     function () {
-      if (repos.length > 0) {
-        setLoadingComponent("");
-        setTotalRepos(repos.length);
-        setFirstTimeLoading(false);
+      if (firstTimeLoading == true) {
+        if (repos.length > 0) {
+          setLoadingComponent("");
+          setTotalRepos(repos.length);
+          setFirstTimeLoading(false);
+        }
       }
     },
     [repos]
